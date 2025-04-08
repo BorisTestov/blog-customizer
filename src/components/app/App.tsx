2e3d6fc9dffd88a8453c78030a4b526cbfa229f7
BorisTestov <boris@the-boris.com>
2025-04-08 13:34:15 +0300

    refactor(app): move app to components

---
+++ src/components/app/App.tsx
@@ -0,0 +1,42 @@
+import { CSSProperties, useState } from 'react';
+import clsx from 'clsx';
+
+import { Article } from 'components/article';
+import { ArticleParamsForm } from 'components/article-params-form';
+import { defaultArticleState } from 'src/constants/articleProps';
+
+import 'src/styles/index.scss';
+import styles from 'src/styles/index.module.scss';
+import { ArticleState } from 'components/article-params-form/ArticleParamsForm';
+
+export const App = () => {
+	const [articleState, setArticleState] = useState(defaultArticleState);
+
+	const handleApplyStyles = (formState: ArticleState) => {
+		setArticleState(formState);
+	};
+
+	const handleResetStyles = () => {
+		setArticleState(defaultArticleState);
+	};
+
+	return (
+		<main
+			className={clsx(styles.main)}
+			style={
+				{
+					'--font-family': articleState.fontFamilyOption.value,
+					'--font-size': articleState.fontSizeOption.value,
+					'--font-color': articleState.fontColor.value,
+					'--container-width': articleState.contentWidth.value,
+					'--bg-color': articleState.backgroundColor.value,
+				} as CSSProperties
+			}>
+			<ArticleParamsForm
+				onApply={handleApplyStyles}
+				onReset={handleResetStyles}
+			/>
+			<Article />
+		</main>
+	);
+};
